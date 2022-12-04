495a01e2d65c77aec614d23e604ad4cf6bb9d7b1
Minhtruong123 <duongminhtruong1234@gmail.com>
2022-12-04 21:23:12 +0700

    Update

--- src/ss3/baitap/ThemPhanTuVaoMang.java
+++ src/ss3/baitap/ThemPhanTuVaoMang.java
@@ -1,7 +1,19 @@
 package ss3.baitap;
 
+import java.util.Scanner;
+
 public class ThemPhanTuVaoMang {
     public static void main(String[] args) {
-        
+        Scanner sc=new Scanner(System.in);
+//        System.out.println("Nhập số lượng phần tử bạn muốn: ");
+//        String n=sc.nextLine();
+        int[] array=new int[3];
+        for (int i=0;i<array.length;i++){
+            array[i]=sc.nextInt();
+        }
+        array[3]=12;
+        for (int i=0;i<array.length;i++){
+            System.out.println(array[i]);
+        }
     }
 }
