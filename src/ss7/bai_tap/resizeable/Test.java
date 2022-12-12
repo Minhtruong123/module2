3705d253a9bec59fe25343411d17e47e1b2b6226
Minhtruong123 <duongminhtruong1234@gmail.com>
2022-12-12 20:33:21 +0700

    Update

--- src/ss7/bai_tap/resizeable/Test.java
+++ src/ss7/bai_tap/resizeable/Test.java
@@ -4,32 +4,32 @@ import ss4.thuchanh.Rectangle;
 
 public class Test {
     public static void main(String[] args) {
-        Geometry[] geometries= new Geometry[3];
-        geometries[0] = new Circle(true,3);
-        geometries[1]= new Recctangle(false,5,10);
-        geometries[2]= new Square(true,4);
-        for (Geometry geometry:geometries){
-            if (geometry instanceof Circle){
+        Geometry[] geometries = new Geometry[3];
+        geometries[0] = new Circle(true, 3);
+        geometries[1] = new Recctangle(false, 5, 10);
+        geometries[2] = new Square(true, 4);
+        for (Geometry geometry : geometries) {
+            if (geometry instanceof Circle) {
                 Resizeable squareInterface = (Circle) geometry;
-                System.out.println("Diện tích ban đầu là: "+squareInterface.getArea());
-                double percent = Math.floor(Math.random()*100+1);
-                System.out.println("Tỉ lệ phần trăm tăng: "+percent+'%');
+                System.out.println("Diện tích ban đầu là: " + squareInterface.getArea());
+                double percent = Math.floor(Math.random() * 100 + 1);
+                System.out.println("Tỉ lệ phần trăm tăng: " + percent + '%');
                 squareInterface.resize(percent);
             }
             System.out.println();
-            if (geometry instanceof Recctangle){
+            if (geometry instanceof Recctangle) {
                 Resizeable recctangleInterface = (Recctangle) geometry;
-                System.out.println("Diện tích ban đầu là: "+recctangleInterface.getArea());
-                double percent = Math.floor(Math.random()*100+1);
-                System.out.println("Tỉ lệ phần trăm tăng: "+percent+'%');
+                System.out.println("Diện tích ban đầu là: " + recctangleInterface.getArea());
+                double percent = Math.floor(Math.random() * 100 + 1);
+                System.out.println("Tỉ lệ phần trăm tăng: " + percent + '%');
                 recctangleInterface.resize(percent);
             }
             System.out.println();
-            if (geometry instanceof Square){
+            if (geometry instanceof Square) {
                 Resizeable squareInterface = (Square) geometry;
-                System.out.println("Diện tích ban đầu là: "+squareInterface.getArea());
-                double percent = Math.floor(Math.random()*100+1);
-                System.out.println("Tỉ lệ phần trăm tăng: "+percent+'%');
+                System.out.println("Diện tích ban đầu là: " + squareInterface.getArea());
+                double percent = Math.floor(Math.random() * 100 + 1);
+                System.out.println("Tỉ lệ phần trăm tăng: " + percent + '%');
                 squareInterface.resize(percent);
             }
         }
