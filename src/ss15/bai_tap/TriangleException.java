8d3b7c1b2c8de064fd6052aedf17c8f9f5bdbc6e
Minhtruong123 <duongminhtruong1234@gmail.com>
2022-12-21 10:56:57 +0700

    Update

--- src/ss15/bai_tap/TriangleException.java
+++ src/ss15/bai_tap/TriangleException.java
@@ -6,23 +6,27 @@ public class TriangleException {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        TriangleExceptionCheck triangleExceptionCheck = new TriangleExceptionCheck();
-
+        TriangleException triangleException = new TriangleException();
+        System.out.println("Nhập cạnh a: ");
+        double a = Integer.parseInt(sc.nextLine());
+        System.out.println("Nhập cạnh b: ");
+        double b = Integer.parseInt(sc.nextLine());
+        System.out.println("Nhập cạnh c: ");
+        double c = Integer.parseInt(sc.nextLine());
         try {
-            System.out.println("Nhập cạnh a: ");
-            double a = Integer.parseInt(sc.nextLine());
-            System.out.println("Nhập cạnh b: ");
-            double b = Integer.parseInt(sc.nextLine());
-            System.out.println("Nhập cạnh c: ");
-            double c = Integer.parseInt(sc.nextLine());
-            try {
-                triangleExceptionCheck.checkTriangleException(a, b, c);
-            } catch (IllegalTriangleException ex) {
-                System.out.println(ex.getMessage());
-            }
-        } catch (Exception e) {
-            System.out.println("Nhập sai!");
+            triangleException.checkTriangleException(a, b, c);
+        } catch (IllegalTriangleException ex) {
+            System.out.println(ex.getMessage());
         }
-        sc.close();
+    }
+
+    public void checkTriangleException(double a, double b, double c) throws IllegalTriangleException {
+        if (a <= 0 || b <= 0 || c <= 0) {
+            throw new IllegalTriangleException("Lỗi: Cạnh tam giác phải dương");
+        } else if (a + b <= c || a + c <= b || b + c <= a) {
+            throw new IllegalTriangleException("Lỗi: Cạnh tam giác không hợp lệ!");
+        } else {
+            System.out.println("Cạnh tam giác hợp lệ");
+        }
     }
 }
